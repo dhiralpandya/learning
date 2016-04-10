84d8c7d117e5c20e0f65cc45386fb60c9eb81180
dhiralpandya <dhiralpandya@gmail.com>
2016-04-10 12:38:43 -0700

    Find Unique Chars In String

--- Learn/src/com/omt/learn/algo/UniqueString.java
+++
@@ -1,5 +0,0 @@
-package com.omt.learn.algo;
-
-public class UniqueString {
-
-}
---
+++ Learn/src/com/omt/learn/algo/UniqueStringExample.java
@@ -0,0 +1,43 @@
+package com.omt.learn.algo;
+
+public class UniqueStringExample {
+
+	/**
+	 * Check String contains unique character or not, Assume that all character
+	 * are ASCII
+	 *
+	 * @param args
+	 */
+	public static void main(String args[]) {
+
+		System.out.println(isStringHasAllUniqueChar("abc"));
+
+	}
+
+	/**
+	 * 
+	 * @param s
+	 * @return
+	 */
+	private static boolean isStringHasAllUniqueChar(String s) {
+		// Again we are assuming that String has only ASCII characters
+		if (s.length() > 256) {
+			return false;
+		}
+
+		boolean[] b = new boolean[256];
+		char[] chArray = s.toCharArray();
+
+		for (int count = 0; count < chArray.length; count++) {
+
+			int i = chArray[count];
+			if (b[i]) {
+				return false;
+			}
+
+			b[i] = true;
+		}
+		return true;
+	}
+
+}

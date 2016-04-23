567f1298e5060e1cf6437a5cda3cecbc184e9303
dhiralpandya <dhiralpandya@gmail.com>
2016-04-22 23:57:26 -0700

    Added new algo

---
+++ Learn/src/com/omt/learn/algo/SeparateDigitInt.java
@@ -0,0 +1,31 @@
+package com.omt.learn.algo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SeparateDigitInt {
+
+	public static void main(String[] args) {
+
+		for (int i : separateDigit(1203)) {
+			System.out.println(i);
+		}
+
+	}
+
+	public static List<Integer> separateDigit(int input) {
+		List<Integer> list = new ArrayList<>();
+
+		if (input > 9) {
+			int digit = input / 10;
+			list.addAll(separateDigit(digit));
+			int carry = input % 10;
+			list.addAll(separateDigit(carry));
+		} else {
+			list.add(input);
+		}
+
+		return list;
+	}
+
+}

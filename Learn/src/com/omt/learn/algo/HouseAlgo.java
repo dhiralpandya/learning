5b6b48a4a7654847a6f2ee0abd7cf7e1a0b34911
dhiralpandya <dhiralpandya@gmail.com>
2016-05-13 15:15:27 -0700

    new algo

---
+++ Learn/src/com/omt/learn/algo/HouseAlgo.java
@@ -0,0 +1,22 @@
+package com.omt.learn.algo;
+
+public class HouseAlgo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a[] = { 8, 5, 10, 40, 50, 35 };
+		System.out.println(max(a, 0));
+	}
+
+	public static int max(int a[], int i) {
+		if (i >= a.length) {
+			return 0;
+		}
+		int f = a[i] + max(a, i + 2);
+		int s = a[i] + max(a, i + 3);
+		int t = max(a, i + 1);
+
+		return Math.max(Math.max(f, s), t);
+	}
+
+}

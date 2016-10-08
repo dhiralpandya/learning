f3f16d24c0883944da76ab6e7efff1a3acf7b5ce
dhiralpandya <dhiralpandya@gmail.com>
2016-10-07 22:58:35 -0700

    new algo

---
+++ Learn/src/com/omt/learn/algo/CircularArrayRotation.java
@@ -0,0 +1,58 @@
+package com.omt.learn.algo;
+
+import java.util.Scanner;
+
+public class CircularArrayRotation {
+
+	public static void main(String[] args) {
+
+		Scanner sn = new Scanner(System.in);
+		String[] lineOne = sn.nextLine().split("\\s+");
+		String[] lineTwo = sn.nextLine().split("\\s+");
+
+		int size = Integer.parseInt(lineOne[0]);
+		int rotate = Integer.parseInt(lineOne[1]);
+		int querySize = Integer.parseInt(lineOne[2]);
+
+		int[] qInt = new int[querySize];
+
+		for (int i = 0; i < querySize; i++) {
+			qInt[i] = Integer.parseInt(sn.nextLine());
+		}
+
+		for (int position : qInt) {
+			System.out.println("Poositions:" + position);
+			System.out.println(find(lineTwo, rotate, position, size));
+		}
+
+	}
+
+	public static int find(String a[], int rotation, int position, int size) {
+
+		int index = size - rotation + position;
+
+		return getElement(a, index, size);
+	}
+
+	public static int getElement(String a[], int index, int size) {
+		if (index == size) {
+			return Integer.parseInt(a[0]);
+		} else if (index > size) {
+			int div = Math.abs(index) / size;
+			index -= (div * size);
+			return getElement(a, index, size);
+		} else if (index < 0) {
+			if (Math.abs(index) > size) {
+				int div = Math.abs(index) / size;
+				index += (div * size);
+			} else {
+				index += size;
+			}
+			return getElement(a, index, size);
+		} else {
+			return Integer.parseInt(a[index]);
+		}
+
+	}
+
+}
---
+++ Learn/src/com/omt/learn/algo/SubsetSumProblem.java
@@ -0,0 +1,27 @@
+package com.omt.learn.algo;
+
+public class SubsetSumProblem {
+
+	public static void main(String[] args) {
+		int a[] = { 2, 3, 7, 8, 10 };
+		int find = 5;
+		System.out.println(isFound(a, 0, find));
+
+	}
+
+	public static boolean isFound(int a[], int i, int remainingFind) {
+
+		if (i >= a.length) {
+			return false;
+		}
+		if (remainingFind == 0) {
+			return true;
+		}
+
+		boolean v1 = false || isFound(a, i + 1, remainingFind - a[i]);
+		boolean v2 = false || isFound(a, i + 1, remainingFind);
+
+		return v1 || v2;
+	}
+
+}

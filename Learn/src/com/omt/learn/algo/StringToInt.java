c5d7ac38f25263cb4d075577b201180fbd12f90e
dhiralpandya <dhiralpandya@gmail.com>
2017-03-10 15:56:04 -0800

    Added new files for IntelliJ Format code

--- Learn/src/com/omt/learn/algo/StringToInt.java
+++ Learn/src/com/omt/learn/algo/StringToInt.java
@@ -2,30 +2,30 @@ package com.omt.learn.algo;
 
 public class StringToInt {
 
-	public StringToInt() {
-		// TODO Auto-generated constructor stub
-	}
+    public StringToInt() {
+        // TODO Auto-generated constructor stub
+    }
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		int a = '0';
-		System.out.println("Zero At :" + a);
+        int a = '0';
+        System.out.println("Zero At :" + a);
 
-		a = '9';
-		System.out.println("Nine At :" + a);
+        a = '9';
+        System.out.println("Nine At :" + a);
 
-		String s = "10923";
+        String s = "10923";
 
-		int d = 0;
-		for (int i = 0; i < s.length(); i++) {
-			int dig = Character.digit(s.charAt(i), 10);
-			System.out.println(dig);
-			d *= 10;
-			d -= dig;
-		}
+        int d = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int dig = Character.digit(s.charAt(i), 10);
+            System.out.println(dig);
+            d *= 10;
+            d -= dig;// This is IMP line
+        }
 
-		d = -d;
-		System.out.println(d);
-	}
+        d = -d;
+        System.out.println(d);
+    }
 
 }

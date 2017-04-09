d43aada785a8379dcee7c193367cfe15795a3195
dhiralpandya <dhiralpandya@gmail.com>
2017-04-09 14:28:02 -0700

    Added new Algorithm 23 March

--- Learn/src/com/omt/learn/algo/util/matrix/MatrixUtil.java
+++ Learn/src/com/omt/learn/algo/util/matrix/MatrixUtil.java
@@ -1,16 +1,34 @@
 package com.omt.learn.algo.util.matrix;
 
 public class MatrixUtil {
-	public static void printMatrix(int[][] i) {
-		for (int[] p : i) {
-			for (int px : p) {
-				if (px > 9) {
-					System.out.print(px + " ");
-				} else {
-					System.out.print(px + "  ");
-				}
-			}
-			System.out.println();
-		}
-	}
+    public static void printMatrix(int[][] i) {
+        for (int[] p : i) {
+            for (int px : p) {
+                if (px > 9) {
+                    System.out.print(px + " ");
+                } else {
+                    System.out.print(px + "  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static int[][] createMatrix(int m, int n) {
+        int[][] matrix = new int[m][n];
+
+
+        int count = 1;
+        for (int raw = 0; raw < m; raw++) {
+
+            for (int column = 0; column < n; column++) {
+                matrix[raw][column] = count++;
+            }
+
+        }
+
+
+        return matrix;
+    }
+
 }

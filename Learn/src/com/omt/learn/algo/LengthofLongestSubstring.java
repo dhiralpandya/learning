6a85586f4cba348830e0f9ef0c1d9514410b5f59
Dhiral Pandya <dhiralpandya@gmail.com>
2017-12-03 23:59:10 -0800

    Just made some changes

--- Learn/src/com/omt/learn/algo/LengthofLongestSubstring.java
+++ Learn/src/com/omt/learn/algo/LengthofLongestSubstring.java
@@ -1,8 +1,17 @@
 package com.omt.learn.algo;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
+import java.util.Stack;
 import java.util.TreeMap;
+
+import javax.print.attribute.HashPrintJobAttributeSet;
 
 class SubStringData {
 	public int start = 0;
@@ -29,9 +38,53 @@ public class LengthofLongestSubstring {
 		for (String subString : getListOfLongestSubString("zabasocac")) {
 			System.out.println(subString);
 		}
+		
+		
+		System.out.println(getLenghtOfSubString("zabasocac"));
+		System.out.println(lengthOfLongestSubstring("zabasocac"));
+
+		
 
 	}
 
+	
+	public static int getLenghtOfSubString(String s) {
+		Queue<Character> queue = new LinkedList<Character>();
+		
+		
+		
+		int max = 0;
+		
+		for(Character c : s.toCharArray()) {
+			
+			while(queue.contains(c)) {
+				queue.poll();
+			}
+			queue.add(c);
+			
+			max = Math.max(max, queue.size());
+		}
+		
+		return max;
+	}
+	
+	
+	public static int getLenghtOfSubStringWithList(String s) {
+		Queue<Character> queue = new LinkedList<Character>();
+		
+		int max = 0;
+		Map<Integer,List<String>> lengthStringList = new HashMap<Integer, List<String>>();
+		for(Character c : s.toCharArray()) {
+			max = Math.max(max, queue.size());
+			while(queue.contains(c)) {
+				queue.poll();
+			}
+			queue.add(c);
+		}
+		
+		return max;
+	}
+	
 	public static int getLength(String s) {
 		int current = 0;
 		int max = 0;
@@ -56,6 +109,23 @@ public class LengthofLongestSubstring {
 		}
 		return max;
 	}
+	
+	 public static int lengthOfLongestSubstring(String s) {
+	        int n = s.length();
+	        Set<Character> set = new HashSet<>();
+	        int ans = 0, i = 0, j = 0;
+	        while (i < n && j < n) {
+	            // try to extend the range [i, j]
+	            if (!set.contains(s.charAt(j))){
+	                set.add(s.charAt(j++));
+	                ans = Math.max(ans, j - i);
+	            }
+	            else {
+	                set.remove(s.charAt(i++));
+	            }
+	        }
+	        return ans;
+	    }
 
 	public static List<String> getListOfLongestSubString(String s) {
 		int current = 0;

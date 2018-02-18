468a549781b8af3101f0fc9356cf7e4eaccbbcb7
dhira <dhira@omt>
2018-02-17 17:37:53 -0800

    Some changes

--- Learn/src/com/omt/learn/practice/SearchInsertPosition.java
+++ Learn/src/com/omt/learn/practice/SearchInsertPosition.java
@@ -5,7 +5,7 @@ public class SearchInsertPosition {
 	public static void main(String[] args) {
 
 		int a[] = { 1, 2, 4, 6, 7, 8, 9, 11, 12 };
-		int target = 0;
+		int target = 10;
 
 		System.out.println(indexToInsert(a, target));
 
@@ -23,15 +23,11 @@ public class SearchInsertPosition {
 				temp = mid;
 				end = mid - 1;
 			} else if (target > a[mid]) {
-				temp = mid;
+				temp = mid + 1;
 				start = mid + 1;
 			} else {
 				return mid;
 			}
-		}
-
-		if (target > a[temp]) {
-			temp += 1;
 		}
 
 		return temp;
